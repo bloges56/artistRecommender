12ea242db2233da20890b2e47265e17d260fed9b
bloges56 <brady.drew.logan@gmail.com>
2023-05-13 12:42:43 -0500

    aded static url path

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from flask.helpers import send_from_directory
 from flask_cors import CORS, cross_origin
 
 
-app = Flask(__name__, static_folder='client/build')
+app = Flask(__name__, static_folder='client/build', static_url_path='')
 CORS(app)
 
 @app.route("/members")
