547d2accd938fae48799dde5cb8d66b5058fdbab
bloges56 <brady.drew.logan@gmail.com>
2023-05-13 11:49:41 -0500

    first commit

---
+++ flask-server/server.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask.helpers import send_from_directory
+from flask_cors import CORS, cross_origin
+
+
+app = Flask(__name__, static_folder='../client/build')
+CORS(app)
+
+@app.route("/members")
+@cross_origin()
+def members():
+    return {"members": ["Member1", "Member2", "Member3"]}
+
+@app.route('/')
+@cross_origin
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
